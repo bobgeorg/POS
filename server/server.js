d1604aff56c799533ca1bc2da7326f5c523edec0
george_bobotas <georgebobotas27@gmail.com>
2025-12-29 15:00:08 +0200

    Update package-lock.json and server.js

--- server/server.js
+++ server/server.js
@@ -28,9 +28,9 @@ app.use('/revenue', revenueRoutes);
 // app.get("/", (req, res) => res.send("Hello all!"));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
